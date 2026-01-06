9c47829d0abbe5062657dac6b5e129461bc9a3dd
bilguundalaim <bilguundalai3355@gmail.com>
2026-01-06 21:21:31 +0800

    simple note

--- popup.js
+++ popup.js
@@ -1,6 +1,31 @@
-console.log('Script loaded');
+const noteList = document.getElementById('noteList');
+
+const displayNotes = (notes) => {
+  noteList.innerHTML = notes.map((note) => 
+    `<div>${note}</div>`
+  ).join('');
+};
 
 document.getElementById('saveBtn').addEventListener('click', () => {
-  const text = document.getElementById('noteArea').value;
-  console.log(`Text: ${text}`)
+  const textArea = document.getElementById('noteArea');
+  const text = textArea.value
+  if (!text) return;
+
+  chrome.storage.local.get(["notes"]).then((result) => {
+    const notes = result.notes || [];
+    notes.push(text);
+
+    chrome.storage.local.set({ notes: notes }).then(() => {
+      console.log("Note saved");
+      displayNotes(notes);
+      textArea.value = '';
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {  
+  chrome.storage.local.get(["notes"]).then((result) => {
+    const savedNotes = result.notes || [];
+    displayNotes(savedNotes);
+  });
 });
